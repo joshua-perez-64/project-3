81ccb24d479220d87e6d3bb2749d03fffb47dc56
joshua-perez-64 <joshtheguardian11@gmail.com>
2024-10-23 14:56:21 -0500

    added gradio integration and added more functions to model.py

--- model.py
+++ model.py
@@ -1,28 +1,38 @@
 import numpy as np
 import pandas as pd
+import os
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import RobustScaler
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-from keras.models import Sequential
-from keras.layers import Dense
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import Dense
+
+def load_data():
+    # Load data
+    csv_path = os.path.join('resources', 'diabetes_data.csv')
+    diabetes_df = pd.read_csv(csv_path)
+    return diabetes_df
 
 def preprocess_data(diabetes_df):
     # Scale BMI
     scaler = RobustScaler()
     diabetes_df['BMI'] = scaler.fit_transform(diabetes_df[['BMI']])
-    
+
     # Split the data into features (X) and target (y)
     X = diabetes_df.drop(columns='Diabetes_binary')
     y = diabetes_df['Diabetes_binary']
-    
+
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
+
     # Undersample to create a balanced dataset
     rus = RandomUnderSampler(random_state=42)
     X_resampled, y_resampled = rus.fit_resample(X_train, y_train)
-    
+
     return X_resampled, y_resampled, X_test, y_test
 
 def build_model(input_shape):
@@ -73,6 +83,27 @@ def main():
     
     # Evaluate the model
     evaluate_model(model, X_test, y_test)
+
+def load_trained_model(model_path='diabetes_model.h5'):
+    return load_model(model_path)
+
+# Preproccess input
+def preprocess_input(data):
+    diabetes_df = pd.read_csv('resources\diabetes_df.csv')
+    # Scale BMI
+    scaler = RobustScaler()
+    diabetes_df['BMI'] = scaler.fit_transform(diabetes_df[['BMI']])
+    return data
+
+def predict(data, model):
+    # Ensure data is in numpy array format
+    input_data = np.array([data])
+
+    # Predict using model
+    prediction = model.predict(input_data)
+
+    # Return the predictions
+    return 'Diabetes' if prediction[0] >= 0.5 else 'No Diabetes'
 
 if __name__ == "__main__":
     main()
